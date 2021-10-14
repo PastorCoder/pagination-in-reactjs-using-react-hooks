363359d7087adf59e18da6259d39adba3211ec7e
pastorcoder <zuma4good@gmail.com>
2021-10-14 18:09:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ReactPaginate from "react-paginate";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [offset, setOffset] = useState(0);
+  const [data, setData] = useState([]);
+  const [perPage] = useState(10);
+  const [pageCount, setPageCount] = useState(0);
+
+  const getData = async () => {
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/photos`);
+    const data = res.data;
+    const slice = data.slice(offset, offset + perPage);
+    const postData = slice.map((pd) => (
+      <div key={pd.id}>
+        <p>{pd.title}</p>
+        <img src={pd.thumbnailUrl} alt="" />
+      </div>
+    ));
+    setData(postData);
+    setPageCount(Math.ceil(data.length / perPage));
+  };
+
+  useEffect(() => {
+    getData()
+  }, [offset])
+
+
+
+  const handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    setOffset(selectedPage + 1)
+};
+
+return (
+ <>
+  <div className="container-fluid">
+    <div className="row">
+      <div className="col-md-5"></div>
+      <div className="col-md-3">
+      <div className="App"><br />
+      <h5>Images from <code>jsonplaceholder</code></h5><br /><br />
+    {data}<br /><br />
+     <ReactPaginate
+                  previousLabel={"back"}
+                  nextLabel={"next"}
+                  breakLabel={"..."}
+                  breakClassName={"break-me"}
+                  pageCount={pageCount}
+                  marginPagesDisplayed={1}
+                  pageRangeDisplayed={4}
+                  onPageChange={handlePageClick}
+                  containerClassName={"pagination"}
+                  subContainerClassName={"pages pagination"}
+                  activeClassName={"active"}/>
+  </div>
+  <div className="col-md-4"></div>
+      </div>
     </div>
-  );
+  </div>
+ </>
+);
 }
 
 export default App;
